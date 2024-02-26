3698fc58ff86c38e0b4c1ee02eb7106cd1735840
wima1605 <wima1605@colorado.edu>
2024-02-26 15:08:04 -0700

    Added bubble chart using react.js and implemented to webpage

---
+++ website/frontend/bubble-chart/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import BubbleChart from './BubbleChart'; 
+import './bubblestyle.css';
+
+
+const App = () => {
+  const bubbleChartData = [
+    { label: 'Sports', value: 100, color: 'red' },
+    { label: 'Health', value: 200, color: 'blue' },
+    { label: 'BingBong', value: 150, color: 'green' },
+   
+  ];
+
+  const handleBubbleClick = (label) => {
+    console.log(`Bubble ${label} is clicked`);
+   
+  };
+
+  const handleLegendClick = (label) => {
+    console.log(`Legend ${label} is clicked`);
+   
+  };
+
+  return (
+    <div>
+      <BubbleChart
+        data={bubbleChartData}
+        bubbleClickFun={handleBubbleClick}
+        legendClickFun={handleLegendClick}
+      />
+    </div>
+  );
+};
+
+export default App;
